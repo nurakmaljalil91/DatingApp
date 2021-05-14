3a8f3fee45f9f0594f7d3dc0d0cc953398ebe5dc
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2021-05-15 06:49:47 +0800

    login logout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import {FormsModule} from "@angular/forms";
     NavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
