d9323191cb7344caa50d50f31069a27d732678d0
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2021-05-29 21:10:09 +0800

    add member interface

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Member} from "../_models/member";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')
+      ? localStorage.getItem('user')! : '{}').token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) {
+  }
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions)
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions)
+  }
+}
