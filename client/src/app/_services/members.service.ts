2bbe867681b210716a016382549d326570f249e5
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2021-06-09 22:58:28 +0800

    add member details

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,15 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Member} from "../_models/member";
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')
-      ? localStorage.getItem('user')! : '{}').token
-  })
-}
 
 @Injectable({
   providedIn: 'root'
@@ -21,10 +14,10 @@ export class MembersService {
   }
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions)
+    return this.http.get<Member[]>(this.baseUrl + 'users')
   }
 
-  getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions)
+  getMember(username: string | null) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
 }
